c0ea059027f716cca8b2af5d0fb1aa66940344e0
Juveria <jfareed7770@gmail.com>
2018-10-09 17:44:45 -0400

    Completed the project

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
 var express = require("express");
 var app = express();
-
-var months = [
-  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-];
-
-var dayNames = {
-				0: "Sun",
-				1: "Mon",
-				2: "Tues",
-				3: "Wed",
-				4: "Thurs",
-				5: "Fri",
-				6: "Sat"
-			};
-
-var locale = "en-us";
+var moment = require("moment");
 
 app.get("/", function(req, res) {
   res.send('<p>Example usage:</p><code>http://localhost:8080/December%2015,%202015</code><br><code>http://localhost:8080/1450137600</code>');
 });
 
-app.get("/:date_string([0-9]*)", function(req, res){
+app.get("/:date_string", function(req, res){
   var dateJson = {"unix":null, "utc":null};
-  var date = new Date(req.params.date_string);
-  var month = date.toLocaleString(locale,{month:"short"});
-  dateJson.unix = date.getTime();
-  dateJson.utc = dayNames[date.getDay()]+ ", "+ date.getUTCDate() + " " + month + " "+ date.getFullYear();
-  res.send(dateJson);
+  var param = req.params.date_string;
+  if(moment(param,["YYYY-MM-DD","MM-DD-YYYY"]).isValid()){
+    var date = moment(param);
+  }else{
+    var date = moment.unix(param);
+ }
+  if(date.isValid())
+  {
+    dateJson.unix = date.valueOf();
+    dateJson.utc = date.format("ddd, MMM Do YYYY, h:mm:ss a");
+    res.send(JSON.stringify(dateJson));
+  }else{
+    res.send('{"error": "Invalid Date"}');
+  }
+
 });
 
 app.listen(8080);
